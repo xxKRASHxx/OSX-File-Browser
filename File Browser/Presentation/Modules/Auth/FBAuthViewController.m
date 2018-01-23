81f9d9b10308e3a0296aaa0571cd0a02a0b26fb2
Danil Lisovoy <danil.lisovoy@sigma.software>
2018-01-24 01:25:32 +0200

    upload-file > improve error handling

--- File Browser/Presentation/Modules/Auth/FBAuthViewController.m
+++ File Browser/Presentation/Modules/Auth/FBAuthViewController.m
@@ -9,16 +9,55 @@
 #import "FBAuthViewController.h"
 
 #import "FBWindowController.h"
+#import "FBAppState.h"
+#import "FBAuthorizationState.h"
 #import "FBSignInAction.h"
 
-@interface FBAuthViewController ()
+@interface FBAuthViewController () <RxSubscriber>
 
 @property (weak) IBOutlet NSTextField *userNameTextField;
 @property (weak) IBOutlet NSSecureTextField *passwordTextField;
+@property (weak) IBOutlet NSTextField *errorLabel;
 
 @end
 
 @implementation FBAuthViewController
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupObserving];
+}
+
+- (void)setupObserving {
+    [MainStore subscribe:self withStateSelector:^id<RxState>(FBAppState *state) {
+        return state.authorization;
+    }];
+}
+
+- (void)dealloc {
+    [MainStore unsubscribe:self];
+}
+
+#pragma mark - <RxSubscriber>
+
+- (void)onStateChanged:(id<FBAuthorizationState>)state {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        guard_ret(![FBAuthorizationStateError cast:state], [self processError:state]);
+        [self processAuthorizing];
+    });
+}
+
+- (void)processError:(FBAuthorizationStateError *)state {
+    self.errorLabel.stringValue = state.error.localizedDescription;
+}
+
+- (void)processAuthorizing {
+    self.errorLabel.stringValue = @"";
+}
+
+#pragma mark - Actions
 
 - (IBAction)loginButtonClicked:(id)sender {
     [MainStore dispatch:[[FBSignInAction alloc]
