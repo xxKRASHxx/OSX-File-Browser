8f6314136776acef8b5f29a263711f49a948fdc6
Danil Lisovoy <danil.lisovoy@sigma.software>
2018-01-24 17:11:12 +0200

    master > clean-up code

--- File Browser/Core/Redux/RxStore.m
+++ File Browser/Core/Redux/RxStore.m
@@ -33,6 +33,8 @@
 
 @implementation RxStore
 
+#pragma mark - Lifecycle
+
 - (instancetype)initWithState:(id<RxState>)rootState reducer:(RxReducer)rootReducer {
     return [self initWithState:rootState reducer:rootReducer middlewares:@[]];
 }
@@ -40,46 +42,55 @@
 - (instancetype)initWithState:(id<RxState>)rootState
                       reducer:(RxReducer)rootReducer
                   middlewares:(NSArray<RxMiddleware>*)middlewares {
-
     self = [super init];
     if (self) {
-
         self.reducer = rootReducer;
         self.state = rootState;
         self.subscriptions = [NSMutableArray array];
-
-        atomic_flag_clear(&_lock);
-        weakify(self);
-
-        RxDispatchFunction defaultDispatch = ^id(id<RxAction> action) {
-            strongify(self);
-            wait(atomic_flag_test_and_set(&_lock));
-            id newState = self.reducer(action, self.state);
-            self.state = newState;
-            [self onStateChanged];
-            atomic_flag_clear(&_lock);
-            return action;
-        };
-
-        RxGetState getState = ^id<RxState>() {
-            strongify(self);
-            return self.state;
-        };
-
-        RxDispatchFunction retryDispatch = ^id(id<RxAction> action) {
-            strongify(self);
-            return [self dispatch:action];
-        };
-
-        self.dispatchFunction =
-        middlewares.reverse.reduce(defaultDispatch, ^id(RxDispatchFunction df, RxMiddleware mw) {
-            return mw([retryDispatch copy], [getState copy])(df);
-        });
-
-        if (self.state == nil) { NSAssert(0, nil); }
+        [self initialSetup:middlewares];
     }
 
     return self;
+}
+
+- (void)initialSetup:(NSArray<RxMiddleware> *)middlewares {
+
+    atomic_flag_clear(&_lock);
+    weakify(self);
+
+    RxDispatchFunction defaultDispatch = ^id(id<RxAction> action) {
+        strongify(self);
+        wait(atomic_flag_test_and_set(&_lock));
+        self.state = self.reducer(action, self.state);
+        atomic_flag_clear(&_lock);
+        return action;
+    };
+
+    RxGetState getState = ^id<RxState>() {
+        strongify(self);
+        return self.state;
+    };
+
+    RxDispatchFunction retryDispatch = ^id(id<RxAction> action) {
+        strongify(self);
+        return [self dispatch:action];
+    };
+
+    self.dispatchFunction =
+    middlewares.reverse.reduce(defaultDispatch, ^id(RxDispatchFunction df, RxMiddleware mw) {
+        return mw([retryDispatch copy], [getState copy])(df);
+    });
+
+    if (self.state == nil) { NSAssert(NO, @"should have initial state."); }
+}
+
+#pragma mark - Override
+
+- (void)setState:(id<RxState>)state {
+    [self willChangeValueForKey:@"state"];
+    _state = state;
+    [self onStateChanged];
+    [self didChangeValueForKey:@"state"];
 }
 
 #pragma mark - <RxStore>
@@ -89,20 +100,19 @@
 }
 
 - (void)subscribe:(id<RxSubscriber>)subscriber withStateSelector:(RxStateSelector)stateSelector {
-    if([self isNewSubscriber:subscriber]) {
+    if ([self isNewSubscriber:subscriber]) {
         RxSubscription *sub = [[RxSubscription alloc] init];
         sub.subscriber = subscriber;
         sub.stateSelector = stateSelector;
         [self.subscriptions addObject:sub];
     }
-    if(self.state) {
+    if (self.state) {
         [subscriber onStateChanged:stateSelector?stateSelector(self.state) : self.state];
     }
 }
 
 - (void)unsubscribe:(id<RxSubscriber>)subscriber {
-    RxSubscription *sub = [self subscriptionWithSubscriber:subscriber];
-    [self.subscriptions removeObject:sub];
+    [self.subscriptions removeObject:[self subscriptionWithSubscriber:subscriber]];
 }
 
 - (id)dispatch:(id<RxAction>)action {
@@ -116,21 +126,16 @@
 }
 
 - (RxSubscription *)subscriptionWithSubscriber:(id<RxSubscriber>)subscriber {
-    for (RxSubscription *sub in self.subscriptions) {
-        if(sub.subscriber == subscriber) {
-            return sub;
-        }
-    }
-    return nil;
+    return [self.subscriptions find:^BOOL(RxSubscription *subscription) {
+        return subscription.subscriber == subscriber;
+    }];
 }
 
 - (void)onStateChanged {
-    for(RxSubscription *sub in self.subscriptions) {
-        if(sub.subscriber) {
-            id<RxState> state = sub.stateSelector? sub.stateSelector(self.state) : self.state;
-            [sub.subscriber onStateChanged:state];
-        }
-    }
+    [self.subscriptions each:^(RxSubscription *sub) {
+        id<RxState> state = sub.stateSelector? sub.stateSelector(self.state) : self.state;
+        [sub.subscriber onStateChanged:state];
+    }];
 }
 
 @end
